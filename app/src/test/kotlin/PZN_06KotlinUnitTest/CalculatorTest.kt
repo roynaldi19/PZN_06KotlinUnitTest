461ba6257d60bf8d35fa620379316276d5ea850b
roynaldi19 <roynaldi19@gmail.com>
2022-01-08 19:31:01 +0700

    comit 5

--- app/src/test/kotlin/PZN_06KotlinUnitTest/CalculatorTest.kt
+++ app/src/test/kotlin/PZN_06KotlinUnitTest/CalculatorTest.kt
@@ -1,10 +1,26 @@
 package PZN_06KotlinUnitTest
 
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.opentest4j.TestAbortedException
 
+@DisplayName("Test for Calculator")
 class CalculatorTest {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll(){
+            println("Sebelum semua unit test")
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun afterAll(){
+            println("Setelah semua unit test")
+        }
+    }
+
     val calculator = Calculator()
 
     @Test
@@ -15,6 +31,12 @@ class CalculatorTest {
 
     @Test
     fun testAddSuccess2() {
+        val result = calculator.add(10,20)
+        assertEquals(20, result, "Hasilnya harusnya 20")
+    }
+    @Test
+    @Disabled("Dimatikan test ini")
+    fun testAddSuccessComingSoon() {
         val result = calculator.add(10,20)
         assertEquals(20, result, "Hasilnya harusnya 20")
     }
@@ -30,6 +52,15 @@ class CalculatorTest {
         assertThrows<IllegalArgumentException> {
             calculator.divide(100,0)
         }
+    }
+
+    @Test
+    fun testAborted() {
+        val profile = System.getenv()["PROFILE"]
+        if ("DEV" != profile) {
+            throw TestAbortedException()
+        }
+        println("Test Not Aborted because Dev Profile")
     }
 
 }
